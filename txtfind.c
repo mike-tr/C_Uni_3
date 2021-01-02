28d2a083e005f5ac29ba05543728185213c940b0
avocado <miket9717@gmail.com>
2021-01-02 11:48:17 +0200

    almost

--- txtfind.c
+++ txtfind.c
@@ -4,24 +4,15 @@
 int main() {
     pReader reader = createReader();
 
-    char *cat = "cat";
-    char *test = "bakaca'tcat222";
-    printf("%s test substring of %s ? %d\n", cat, test, substring(test, cat));
-    printf("%s test similar to %s ? %d\n", cat, test, similar(test, cat, 2));
-
-    char *target = "window 2";
-    char *s = "wi2ndofw1 2";
-    printf("%s test similar to %s ? %d\n", s, target, similar(s, target, 3));
-
-    //printAll(reader);
-    print_similar_words(reader, 2, "cat");
-    printf("\n");
-    print_lines(reader, 2, "cat");
-
     char word[WORD];
-    getWordAt(reader, 0, 1, word);
-    printf("\n%s\n", word);
+    getWordAt(reader, 0, 0, word);
+    char symbol[WORD];
+    getWordAt(reader, 0, 1, symbol);
 
-    printf("%s test substring of %s ? %d\n", cat, "cat", substring("cat", cat));
+    printf("\n%s : %s\n", word, symbol);
+
+    print_similar_words(reader, 2, word);
+    printf("\n");
+    print_lines(reader, 2, word);
     return 0;
 }
